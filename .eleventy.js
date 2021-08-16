b6d75a6427c8396a6143ec25a63d924984bbafab
Tony Grimes <acidtone@tonygrimes.com>
2021-08-15 21:45:14 -0600

    added schedule

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,15 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/images');
   eleventyConfig.addPassthroughCopy('src/css');
   eleventyConfig.addPassthroughCopy('src/js');
+  
+  eleventyConfig.addFilter("readablePostDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {
+        zone: "Europe/Amsterdam",
+    }).setLocale('en').toFormat('LLL d');
+  });
 
   return {
     dir: {
